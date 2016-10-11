ccf11593d2171d7a11cb70dd6a49192fdf395ca5
HanWeiguang <hanweiguang1@gmail.com>
2016-10-11 15:40:53 +0800

    添加了data.py、pipeline.py，还没有完成这两个文件的单元测试 一个能够向数据库写入爬取的数据的初步版本

--- stock_price_crawler/models/data.py
+++ stock_price_crawler/models/data.py
@@ -1,2 +1,45 @@
 # -*- coding: utf-8 -*-
+import logging
+from sqlalchemy import Column
+from sqlalchemy import DateTime
+from sqlalchemy import MetaData
+from sqlalchemy import String
+from sqlalchemy import Table
+from sqlalchemy.exc import IntegrityError
+from stock_price_crawler.database import create_engine_with, DBConnection
 
+logger = logging.getLogger(__name__)
+
+
+class ItemTable(object):
+    engine = create_engine_with()
+
+    def __init__(self):
+        self.table = Table(
+            'item_table',
+            MetaData(),
+            Column('stock_market', String, primary_key=True),
+            Column('stock_market_link', String),
+            Column('scrape_time', DateTime),
+        )
+
+    def insert(self, **kwargs):
+        try:
+            with DBConnection(self.engine) as connection:
+                connection.execute(self.table.insert().values(**kwargs))
+        except IntegrityError, e1:
+            logger.info(e1.message)
+            if 'Duplicate' in e1.message:
+                logger.info('Records exist')
+                stock_market = kwargs.pop('stock_market')
+                update = self.table.update()\
+                    .where(self.table.c.stock_market == stock_market)\
+                    .values(**kwargs)
+                try:
+                    with DBConnection(self.engine) as connection:
+                        connection.execute(update)
+                    logger.info('Records update')
+                except BaseException, e2:
+                    logger.exception(e2)
+            else:
+                logger.exception(e1)
--- stock_price_crawler/pipelines.py
+++ stock_price_crawler/pipelines.py
@@ -4,8 +4,20 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import time
+
+from stock_price_crawler.models.data import ItemTable
 
 
 class StockPriceCrawlerPipeline(object):
     def process_item(self, item, spider):
+        data = dict()
+        data['stock_market'] = item['stock_market']
+        data['stock_market_link'] = item['stock_market_link']
+        data['scrape_time'] = time.strftime(
+            "%Y-%m-%d %H:%M:%S",
+            time.localtime()
+        )
+        table = ItemTable()
+        table.insert(**data)
         return item
